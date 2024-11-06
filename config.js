45c390705225a9e4fbdcf9d58a45b66e8709a027
Joseph Adams <josephdadams@gmail.com>
2024-11-06 13:59:06 -0500

    simplify wording

--- config.js
+++ config.js
@@ -9,16 +9,7 @@ export const ConfigFields = [
 		id: 'info',
 		label: 'Information',
 		width: 12,
-		value: `
-				<div class="alert alert-danger">
-					<h3>IMPORTANT MESSAGE</h3>
-					<div>
-						<strong>Please read and understand the following before using this module</strong>
-						<br>
-						This software is for use as a stopwatch, making it easier to manipulate times through the companion.
-					</div>
-				</div>
-			`,
+		value: `This software controls "CronosDown" by Monteiro, for use as a stopwatch.`,
 	},
 	{
 		type: 'textinput',
